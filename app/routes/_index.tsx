820598530884f215c9e33a02fbe2bd8d8d6a71b3
JaydenWN <123134488+JaydenWN@users.noreply.github.com>
2024-04-14 12:46:48 +1000

    card

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
-import { Paper } from "@mantine/core";
+import { Paper, SimpleGrid } from "@mantine/core";
 import type { MetaFunction } from "@remix-run/node";
 import CardComp from '../components/CardComp'
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,37 +10,39 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export default function Index() {
-  return (
-    <Paper>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+export async function loader(){
+  const response = await fetch('https://sea-lion-app-6er6u.ondigitalocean.app/api/posts')
 
-      <CardComp/>
-    </Paper>
-  );
+  const {data} = await response.json()
+  console.log(data)
+  
+  return data
+}
+
+export default function Index() {
+
+  const loaderData = useLoaderData()
+
+  console.log(loaderData)
+
+  if(loaderData[0]){
+    return (
+      <div>
+        <h1>View Blog Posts</h1>
+        <SimpleGrid cols={{base: 1, md: 3}}>
+        {loaderData.map((post)=>(
+          <CardComp key={post.attributes.title} title={post.attributes.title}/>
+        ))}
+        </SimpleGrid>
+      </div>
+    );
+  }
+  else{
+    return(
+      <div>
+        loading...
+      </div>
+    )
+  }
+
 }
